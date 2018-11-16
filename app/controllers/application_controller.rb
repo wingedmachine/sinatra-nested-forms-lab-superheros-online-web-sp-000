02976507b61bf1b697c02dfc21119313da48b942
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 06:25:40 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
-
-
+  get '/' do
+    erb :super_hero
+  end
 end
