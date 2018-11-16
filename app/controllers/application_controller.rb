d062e5b8df9bb7fcc0a7a91047adb76f071cdbc9
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-11-16 06:41:15 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+  set :views, Proc.new { File.join(root, "../views/") }
 
-    set :views, Proc.new { File.join(root, "../views/") }
+  get '/' do
+    erb :super_hero
+  end
 
-
+  post '/teams' do
+    @team = params[:team]
+    erb :team
+  end
 end
